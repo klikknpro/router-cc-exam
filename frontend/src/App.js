34cb5a4c587cab657b88b71d22ebd15f9a007cf3
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-05-18 18:42:51 +0200

    just notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function App() {
+  const { REACT_APP_MAPBOX_ROUTER } = process.env;
+
   return (
     <div className="App">
       <h1>Hello Router</h1>
+      <div id="mapbox"></div>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+*/
