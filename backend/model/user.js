99e507957bc4faa43a95b7eca22b6a7502f3d253
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-06-16 08:44:52 +0200

    rework the model

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,15 +1,21 @@
 const mongoose = require("mongoose");
 
+// const checkpointSchema = new mongoose.Schema({
+//   routeId: { type: String, required: true, unique: true },
+//   coordinates: { type: String, required: true }, // response.routes[0].legs(iterate!).steps(iterate!).geometry.coordinates[1]
+//   duration: { type: Number, required: true },
+// });
+
 const routeSchema = new mongoose.Schema({
   description: { type: String, required: true }, // input || response.routes[0].legs[0].summary
   uuid: { type: String, required: true }, // response.uuid
   from: { type: String, required: true }, // response.routes[0].geometry.coordinates[0]
   to: { type: String, required: true }, // response.routes[0].geometry.coordinates[lastOne]
-  /* if (waypoints < enough) response.routes[0].geometry.coordinates  */
-  waypoints: { type: Array, required: true }, // response.routes[0].waypoints
+  coordinates: { type: Array, required: true }, // response.routes[0].geometry.coordinates
+  checkpoints: [checkpointSchema],
   distance: { type: Number, required: true }, // response.routes[0].distance
   isPublic: { type: Boolean, default: false },
-  tFactor: { type: Number, required: true },
+  tFactor: { type: Number, required: true }, // Toughness - Factor
 });
 
 const userSchema = new mongoose.Schema({
@@ -24,4 +30,5 @@ const User = mongoose.model("user", userSchema);
 module.exports = User;
 
 /*
+inkabb a "legs"-et hasznalom majd
  */
--- backend/route/dashboard.js
+++ backend/route/dashboard.js
@@ -4,26 +4,25 @@ const auth = require("../middleware/auth");
 
 /* these are REST endpoints */
 
-router.get("/", auth({ block: true }), async (req, res) => {
-  const user = await User.findById(res.locals.user.userId);
-  res.json({ user }); // = {user: user}
-}); // display the user's all dashboards
+router.post("/", auth({ block: true })); // save route to db
 
-// router.get("/:id", auth({ block: true }), dashboardById); // display one dashboard
+router.get("/", auth({ block: true })); // display the user's dashboard
+
+router.get("/:routeId", auth({ block: true })); // display selected route
+
+/* change "description" from body */
+/* set public from /:routeId?isPublic=true */
+router.patch("/:routeId", auth({ block: true })); // update and response with updated document
+
+router.delete("/:routeId", auth({ block: true })); // isDeleted: true ;)
 
 // router.get("/:id/todos", auth({ block: true }), allTodos);
 
 // router.get("/:id/todos/:todoId", auth({ block: true }), todoById);
 
-// router.post("", controller); // create dashboard and send dashboard :id
-
 // router.post("/:id/todos"); // create a todo and send todo :id back
 
-// router.patch("/:id", controller); // update and send updated dashboard :id back
-
 // router.patch("/:id/todos/:id", controller); // update and send updated todo :id back
-
-// router.delete("/:id", controller); // isDeleted: true ;)
 
 // router.delete("/:id/todos/:id", controller); // isDeleted: true ;)
 
