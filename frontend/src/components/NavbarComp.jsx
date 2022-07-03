c04e94dd7a11025d8edd7938d564e8e71301ec97
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-07-03 10:32:17 +0200

    failed bootstrap navbar

--- frontend/src/components/NavbarComp.jsx
+++ frontend/src/components/NavbarComp.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../providers/auth";
-import { Button } from "react-bootstrap";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Button } from "@mui/material";
+import logo from "../img/router_logo.png";
 
 const NavbarComp = () => {
   const navigate = useNavigate();
@@ -15,32 +15,7 @@ const NavbarComp = () => {
   };
 
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
-
-export default NavbarComp;
-
-/*
-<nav className="navbar">
+    <nav className="navbar">
       <Button onClick={() => nav("/")} variant="contained" size="small">
         Home
       </Button>
@@ -57,4 +32,31 @@ export default NavbarComp;
         </Button>
       )}
     </nav>
+  );
+};
+
+export default NavbarComp;
+
+/*
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} width="280" className="d-inline-block align-top" alt="Router Project" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/feed">
+              Feed
+            </Nav.Link>
+            <Nav.Link as={Link} to="/profile">
+              My Profile
+            </Nav.Link>
+            {token ? <Nav.Item onClick={logout}>Logout</Nav.Item> : <Nav.Item onClick={auth}>Google login</Nav.Item>}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 */
